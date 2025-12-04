a4da3cb9dc13786b002856ae44a4d261ee5ed936
Lucas Eduardo dos Santos <89871495+lucsdsm@users.noreply.github.com>
2025-12-04 09:46:51 +0000

    removida pasta carros

--- django/carros/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Carro
-
-@admin.register(Carro)
-class CarroAdmin(admin.ModelAdmin):
-    list_display = ['modelo', 'preco']
-    search_fields = ['modelo']
